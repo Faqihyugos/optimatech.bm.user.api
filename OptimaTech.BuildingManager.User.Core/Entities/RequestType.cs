cdbdedc153423f7d3dcac0edefe13c52b2c60ac8
Faqihyugos <faqihyugos@gmail.com>
2024-11-26 17:05:06 +0700

    feat(Core/Entities): add RequestType and Role entities

---
+++ OptimaTech.BuildingManager.User.Core/Entities/RequestType.cs
@@ -0,0 +1,9 @@
+namespace OptimaTech.BuildingManager.User.Core.Entities;
+
+public class RequestType : IBasic
+{
+    public required Guid Id { get; set; }
+    public required Guid TenantId { get; set; }
+
+    public required Status Name { get; set; }
+}
